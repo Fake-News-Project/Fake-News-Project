4e97724c9ebd4299ab7b86e1a13a04ab1260783f
Jinmei Zhang <jmzhang@vt.edu>
2017-08-28 18:27:12 -0700

    add an API using flask (run with: python run.py)

---
+++ flask_api/run.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+from compute_score import compute_score
+
+app = Flask(__name__)
+
+@app.route('/')
+def my_form():
+  return render_template("my-form.html")
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+  
+  url = request.form['url']
+  # function to compute the score for the probability of being fake news
+  # processed_text = url.upper()
+  test = compute_score(str(url))
+  return test
+
+if __name__ == '__main__':
+  app.run()
+
